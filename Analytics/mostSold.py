a8776f02af9f7274096099d00237c6b20002460e
Cesar Yannick Torres <cesaryannick6@gmail.com>
2023-03-15 12:51:50 -0600

    testing some changes on server

---
+++ Analytics/mostSold.py
@@ -0,0 +1,5 @@
+def showMostSold(products):
+    print("A continuación se muestran los mas vendidos : ")
+    for product in products:
+        print(product)
+    return "Productos impresos en consola"
--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 import socket
+from Analytics.mostSold import showMostSold
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '¡Hola, Mundo!'
+    return showMostSold([{"name": "CocaCola2L", "price": 28}])
 
 if __name__ == '__main__':
     # Obtener la dirección IP del servidor
